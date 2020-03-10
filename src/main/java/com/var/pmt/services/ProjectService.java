421c181942a7492a696f6f958eaa8b40feb17920
mdaftabasmu <mdaftabalam196@gmail.com>
2020-03-10 22:22:31 +0530

    adding controller

---
+++ src/main/java/com/var/pmt/services/ProjectService.java
@@ -0,0 +1,21 @@
+package com.var.pmt.services;
+
+import com.var.pmt.models.Project;
+import com.var.pmt.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        //Logic
+
+        return projectRepository.save(project);
+    }
+
+}
