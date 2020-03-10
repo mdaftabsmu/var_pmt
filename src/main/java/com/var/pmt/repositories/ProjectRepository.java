f460ba9bb1c68d23d68665aff19ff2bd9a96099a
mdaftabasmu <mdaftabalam196@gmail.com>
2020-03-10 22:05:24 +0530

    first commit

---
+++ src/main/java/com/var/pmt/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.var.pmt.repositories;
+
+import com.var.pmt.models.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
