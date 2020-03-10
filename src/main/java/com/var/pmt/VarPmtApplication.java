23220397872df56a6cc4b54dee953cf46efbeee9
mdaftabasmu <mdaftabalam196@gmail.com>
2020-03-10 21:48:55 +0530

    first commit

---
+++ src/main/java/com/var/pmt/VarPmtApplication.java
@@ -0,0 +1,13 @@
+package com.var.pmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarPmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VarPmtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/var/pmt/VarPmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.var.pmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarPmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
